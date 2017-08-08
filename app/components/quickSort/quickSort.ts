7ed5da00fb18b9036970fd127796556e8372e9b1
Nikolay <derzik12@gmail.com>
2017-08-08 22:59:26 +0800

    выводит Uncaught RangeError: Maximum call stack size exceeded

--- app/components/quickSort/quickSort.ts
+++ app/components/quickSort/quickSort.ts
@@ -1,80 +1,81 @@
 export default class quickSort {
-    stringTable: string[] = [
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-        "А",
-        "Б",
-        "В",
-        "Г",
-        "Д",
-        "Е",
-        "Ё",
-        "Ж",
-        "З",
-        "И",
-        "Й",
-        "К",
-        "Л",
-        "М",
-        "Н",
-        "О",
-        "П",
-        "Р",
-        "С",
-        "Т",
-        "У",
-        "Ф",
-        "Х",
-        "Ц",
-        "Ч",
-        "Ш",
-        "Щ",
-        "Ъ",
-        "Ы",
-        "Ь",
-        "Э",
-        "Ю",
-        "Я",
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9"
-    ];
+    stringTable = {
+        "0": "000",
+        "1": "001",
+        "2": "002",
+        "3": "003",
+        "4": "004",
+        "5": "005",
+        "6": "006",
+        "7": "007",
+        "8": "008",
+        "9": "009",
+        "A": "110",
+        "B": "111",
+        "C": "112",
+        "D": "113",
+        "E": "114",
+        "F": "115",
+        "G": "116",
+        "H": "117",
+        "I": "118",
+        "J": "119",
+        "K": "120",
+        "L": "121",
+        "M": "122",
+        "N": "123",
+        "O": "124",
+        "P": "125",
+        "Q": "126",
+        "R": "127",
+        "S": "128",
+        "T": "129",
+        "U": "130",
+        "V": "131",
+        "W": "132",
+        "X": "133",
+        "Y": "134",
+        "Z": "135",
+        "А": "136",
+        "Б": "137",
+        "В": "138",
+        "Г": "139",
+        "Д": "140",
+        "Е": "141",
+        "Ё": "142",
+        "Ж": "143",
+        "З": "144",
+        "И": "145",
+        "Й": "146",
+        "К": "147",
+        "Л": "148",
+        "М": "149",
+        "Н": "150",
+        "О": "151",
+        "П": "152",
+        "Р": "153",
+        "С": "154",
+        "Т": "155",
+        "У": "156",
+        "Ф": "157",
+        "Х": "158",
+        "Ц": "159",
+        "Ч": "160",
+        "Ш": "161",
+        "Щ": "162",
+        "Ъ": "163",
+        "Ы": "164",
+        "Ь": "165",
+        "Э": "166",
+        "Ю": "167",
+        "Я": "168",
+
+};
 
     constructor() {
     }
 
-    private static partition(mas: any[], l: number, r: number, field: string):number {
+    private static partition(mas: any[], l: number, r: number):number {
         function swap(mas: any[], a:any, b:any) {
 
             let tmp: any = mas[a];
@@ -86,21 +87,21 @@ export default class quickSort {
 
         let pos: number = l-1;
         for (let i: number = l; i <= r; ++i) {
-            if (+mas[i].id <= +mas[r][field] )
+            if (+mas[i]["__sortTMP__"] <= +mas[r]["__sortTMP__"] )
                 mas = swap(mas, ++pos, i);
         }
 
         return pos;
     }
 
-    private quick_sort(mas: any[], l: number, r: number, field: string) {
+    private quick_sort(mas: any[], l: number, r: number) {
         if (l >= r) {
             return;
         }
-        let pivot: number = quickSort.partition(mas,l,r,field);
+        let pivot: number = quickSort.partition(mas,l,r);
 
-        this.quick_sort(mas,l,pivot-1,field);
-        this.quick_sort(mas,pivot+1,r,field);
+        this.quick_sort(mas,l,pivot-1);
+        this.quick_sort(mas,pivot+1,r);
     }
 
     getSorted(array: any[], field: any) {
@@ -110,25 +111,21 @@ export default class quickSort {
         if (!field) {
             field = "id"
         }
-        let stringTable: string[] = this.stringTable;
+
+        let stringTable: any = this.stringTable;
 
         array.forEach(function(item, i, arr) {
-            arr[i]["__sortTMP__"] =
-                stringTable.indexOf( item[field].toString().charAt(0).toUpperCase() );
-            debugger
+
+            arr[i]["__sortTMP__"] = item[field].toString().replace(/\s{2,}/g, ' ').replace(/./gi,($0: any)=>stringTable[$0.toUpperCase()]||$0);
+
+            arr[i]["__sortTMP__"] = (+arr[i]["__sortTMP__"] * 0.01) + "";
+            mas[i] = item;
         });
-
-        if (masHaveString.length >= 2) {
-            let l:number = 0;
-            let r:number = masHaveString.length - 1;
-
-            this.quick_sort(masHaveString,l,r,field);
-        }
 
         let l:number = 0;
         let r:number = mas.length - 1;
-        this.quick_sort(mas,l,r,field);
+        this.quick_sort(mas,l,r);
 
-        return mas.concat(masHaveString);
+        return mas;
     }
 }
